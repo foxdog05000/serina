e4690b990324d524151e05c68b616981d9ae8f96
Florian Armand <florianarmand05000@gmail.com>
2017-03-16 22:01:47 +0100

    Ajout angular-material lib

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
