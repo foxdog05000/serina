f227df89cd6d382e7fe4443913b6b3180b17c2c3
Florian Armand <florianarmand05000@gmail.com>
2017-03-16 22:12:50 +0100

    Del fichier inutile + maj dependances bower

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,16 +19,6 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
