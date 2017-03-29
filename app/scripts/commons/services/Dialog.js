af5f44a4c58c94bd7c512fd1f4a6b92397b392e1
Florian Armand <florianarmand05000@gmail.com>
2017-03-29 22:24:04 +0200

    Add prompt on Dialog service

--- app/scripts/commons/services/Dialog.js
+++ app/scripts/commons/services/Dialog.js
@@ -4,14 +4,28 @@ angular.module('serinaApp').factory('Dialog', function ($mdDialog) {
   return {
 
     showConfirm: function (ev) {
-      var confirm = $mdDialog.confirm()
+      var dialog = $mdDialog.confirm()
         .title('Êtes-vous sûr ?')
         .ariaLabel('Are you sure ?')
         .targetEvent(ev)
         .ok('Valider')
         .cancel('Annuler')
 
-      return $mdDialog.show(confirm)
+      return $mdDialog.show(dialog)
+    },
+
+    showPrompt: function (options) {
+      var dialog = $mdDialog.prompt()
+        .title(options.title)
+        .textContent(options.textContent)
+        .placeholder(options.placeholder)
+        .ariaLabel(options.ariaLabel)
+        .initialValue(options.initialValue)
+        .targetEvent(options.ev)
+        .ok(options.ok)
+        .cancel(options.cancel)
+
+      return $mdDialog.show(dialog)
     }
 
   }
