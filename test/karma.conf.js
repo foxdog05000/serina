9231c08433f7f687e665c2edcf348fee43b9a4bd
Florian Armand <florianarmand05000@gmail.com>
2017-06-16 11:30:40 +0200

    Add angular aria + messages for fix import via angular-material

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,11 +22,11 @@ module.exports = function(config) {
       // bower:js
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
