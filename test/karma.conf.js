d8c4599f922bce2de78d35e273a43bc1be44686d
Florian Armand <florianarmand05000@gmail.com>
2017-06-18 22:08:40 +0200

    Remove link import js angular-aria

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
