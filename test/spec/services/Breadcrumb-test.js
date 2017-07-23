69d8f97818529f3e617e0371ffecd890b1e54b86
Florian Armand <florianarmand05000@gmail.com>
2017-07-23 16:58:46 +0200

    MAJ TU Breadcrumb service for passed successful

--- test/spec/services/Breadcrumb-test.js
+++ test/spec/services/Breadcrumb-test.js
@@ -39,24 +39,13 @@ describe('Breadcrumb Service', function () {
     expect(breadcrumbResult).not.toEqual([{ label: 'home', href: '/' }])
   })
 
-  it('should delete level is correct', function () {
-    var currentBreadcrumb = [{ label: 'home', href: '/' }, { label: 'dialog', href: '/lang/en/dialog' }]
-    var groups
-    var breadcrumbResult = Breadcrumb.build(currentBreadcrumb, lang, groups)
-
-    expect(breadcrumbResult).toEqual([{ label: 'home', href: '/' }])
-    expect(breadcrumbResult).not.toEqual([{ label: 'home', href: '/' }, { label: 'dialog', href: '/lang/en/dialog' }])
-    expect(breadcrumbResult).not.toEqual([{ label: 'home', href: '/' }, { label: '', href: '/lang/en' }])
-  })
-
-  // To completed this test
   it('should build 3 level is correct', function () {
     var currentBreadcrumb = Breadcrumb.init('home', '/')
     var groups = 'commons/dialog/action'
     var breadcrumbResult = Breadcrumb.build(currentBreadcrumb, lang, groups)
 
-    expect(breadcrumbResult).toEqual('')
-    expect(breadcrumbResult).not.toEqual('')
-    expect(breadcrumbResult).not.toEqual('')
+    expect(breadcrumbResult).toEqual([{ label: 'home', href: '/' }, { label: 'commons', href: '/lang/en/commons' }, { label: 'dialog', href: '/lang/en/commons/dialog' }, { label: 'action', href: '/lang/en/commons/dialog/action' }])
+    expect(breadcrumbResult).not.toEqual([{ label: 'home', href: '/' }, { label: 'commons', href: '/lang/en/commons' }])
+    expect(breadcrumbResult).not.toEqual([{ label: 'home', href: '/' }])
   })
 })
