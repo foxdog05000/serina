fde6e4aabbb228242e8d0bfa042f3c6071e66a67
Farmand <farmand@netceler.com>
2017-08-22 11:38:17 +0200

    Fix bad param on delete group on level root of translation

--- api/api.js
+++ api/api.js
@@ -126,7 +126,7 @@ app.post(pathApi + '/:lang/group/del', function (req, res) {
   jsonfile.readFile(file, function (err, obj) {
     if (err) { console.log('Error on read json file', err) }
     if (!isDefined(groups)) {
-      delete obj[req.params.groupName]
+      delete obj[req.body.groupName]
     } else {
       let i = 0
       searchGroup(obj, groups, req.body.groupName, 'del', i)
