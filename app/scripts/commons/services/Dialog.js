75aa3f0a94862e81e9b9fb44837065bcdcffa53e
Florian Armand <florianarmand05000@gmail.com>
2017-08-24 20:01:44 +0200

    Maj Dialog service

--- app/scripts/commons/services/Dialog.js
+++ app/scripts/commons/services/Dialog.js
@@ -4,18 +4,17 @@ angular.module('serinaApp').factory('Dialog', function ($mdDialog, $i18next) {
   return {
 
     showConfirm: function (ev) {
-      var dialog = $mdDialog.confirm()
+      const dialog = $mdDialog.confirm()
         .title($i18next.t('commons.dialog.confirmTitle'))
         .ariaLabel($i18next.t('commons.dialog.confirmTitle'))
         .targetEvent(ev)
         .ok($i18next.t('commons.actions.validate'))
         .cancel($i18next.t('commons.actions.cancel'))
-
       return $mdDialog.show(dialog)
     },
 
     showPrompt: function (options) {
-      var dialog = $mdDialog.prompt()
+      const dialog = $mdDialog.prompt()
         .title(options.title)
         .textContent(options.textContent)
         .placeholder(options.placeholder)
@@ -24,7 +23,6 @@ angular.module('serinaApp').factory('Dialog', function ($mdDialog, $i18next) {
         .targetEvent(options.ev)
         .ok(options.ok)
         .cancel(options.cancel)
-
       return $mdDialog.show(dialog)
     }
 
