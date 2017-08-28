b6121c10e13d04cdf4057737703eac2a444bf6dc
Farmand <farmand@netceler.com>
2017-08-28 15:32:20 +0200

    Create advanced setting directive + maj translation en/fr

---
+++ app/scripts/settings/advancedSettings.js
@@ -0,0 +1,11 @@
+'use strict'
+
+angular.module('serinaApp').directive('advancedSettings', function ($rootScope, $mdColorPalette) {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/settings/advanced-settings.html',
+    link: function (scope) {
+
+    }
+  }
+})
