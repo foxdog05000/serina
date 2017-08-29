e90e14cce3fdce97cb1873e89c198c7c22dc9ec8
Farmand <farmand@netceler.com>
2017-08-29 12:04:37 +0200

    Fix bad path of flags images

--- Gruntfile.js
+++ Gruntfile.js
@@ -266,7 +266,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/views/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '!<%= yeoman.dist %>/flags/languages/*',
+          '!<%= yeoman.dist %>/images/flags/*',
           '<%= yeoman.dist %>/views/fonts/*'
         ]
       }
