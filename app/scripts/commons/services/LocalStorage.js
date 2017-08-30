3e90d12b509a06fefb3991e71338b4a86cfb3c7e
Farmand <farmand@netceler.com>
2017-08-30 09:57:53 +0200

    Create service LocalStorage

---
+++ app/scripts/commons/services/LocalStorage.js
@@ -0,0 +1,19 @@
+'use strict'
+
+angular.module('serinaApp').factory('LocalStorage', function () {
+  return {
+
+    itemExist: function (itemKey) {
+      return localStorage.getItem(itemKey) !== null;
+    },
+
+    getItem: function (itemKey) {
+      return JSON.parse(localStorage.getItem(itemKey))
+    },
+
+    setItem: function (itemKey, data) {
+      localStorage.setItem(itemKey, JSON.stringify(data))
+    }
+
+  }
+})
