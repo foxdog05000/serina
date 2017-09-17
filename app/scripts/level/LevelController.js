1387d2ea5aa9157c4140e48116e740248efaef1e
Florian Armand <florianarmand05000@gmail.com>
2017-09-17 19:35:37 +0200

    Add button back to top on page level

--- app/scripts/level/LevelController.js
+++ app/scripts/level/LevelController.js
@@ -1,8 +1,9 @@
 'use strict'
 
-angular.module('serinaApp').controller('LevelCtrl', function ($rootScope, $scope, $routeParams, DataAccessor, Breadcrumb) {
+angular.module('serinaApp').controller('LevelCtrl', function ($rootScope, $scope, $routeParams, $location, $anchorScroll, DataAccessor, Breadcrumb) {
   $rootScope.breadcrumb = Breadcrumb.init($routeParams.language.toUpperCase(), '/language/' + $routeParams.language.toLowerCase())
   var originatorEv
+  var pageOffset = document.documentElement.scrollTop || document.body.scrollTop
 
   var getListGroupsAndTranslations = function (content, levels) {
     $scope.listGroups = []
@@ -26,6 +27,11 @@ angular.module('serinaApp').controller('LevelCtrl', function ($rootScope, $scope
     window.history.back()
   }
 
+  $scope.gotoTop = function () {
+    $location.hash('backToTop')
+    $anchorScroll()
+  }
+
   $scope.openMenu = function ($mdMenu, ev) {
     originatorEv = ev
     $mdMenu.open(ev)
@@ -38,4 +44,13 @@ angular.module('serinaApp').controller('LevelCtrl', function ($rootScope, $scope
   }, function (response) {
     console.error('Error on open language ' + $scope.currentLanguage, response)
   })
+
+  window.onscroll = function () {
+    if(pageOffset >= 300) {
+      document.getElementById('buttonBackToTop').style.visibility = 'hidden'
+    } else {
+      document.getElementById('buttonBackToTop').style.visibility = 'visible'
+    }
+  }
+
 })
