74234c69ccbf195ef40d971def3d7a93e36f2e27
Florian Armand <florianarmand05000@gmail.com>
2017-11-15 21:14:13 +0100

    Fix: Add a new translation displays the save button for all translations

--- app/scripts/level/translation.js
+++ app/scripts/level/translation.js
@@ -8,17 +8,21 @@ angular.module('serinaApp').directive('translation', function ($rootScope, $rout
 
       scope.$watch('listTranslations', function (newValue, oldValue) {
         if (newValue !== oldValue && !angular.isUndefined(oldValue)) {
+          var firstEntitiesNotSave = 0
+          if (!newValue[0].save) {
+            firstEntitiesNotSave = 1
+          }
           for (var iterator = 0; iterator < oldValue.length; iterator++) {
-            if (!angular.isUndefined(newValue[iterator]) && (newValue[iterator].save && oldValue[iterator].save)) {
+            if (!angular.isUndefined(newValue[iterator + firstEntitiesNotSave]) && (newValue[iterator + firstEntitiesNotSave].save && oldValue[iterator].save)) {
               if (!$rootScope.secondLanguageIsValid) {
-                if (angular.equals(newValue[iterator].key, scope.originalListTranslations[iterator].key) && angular.equals(newValue[iterator].value[0], scope.originalListTranslations[iterator].value[0])) {
+                if (angular.equals(newValue[iterator + firstEntitiesNotSave].key, scope.originalListTranslations[iterator].key) && angular.equals(newValue[iterator + firstEntitiesNotSave].value[0], scope.originalListTranslations[iterator].value[0])) {
                   scope.listTranslations[iterator].modified = false
                 } else {
                   scope.listTranslations[iterator].modified = true
                   scope.listTranslations[iterator].originalKey = scope.originalListTranslations[iterator].key
                 }
               } else {
-                if (angular.equals(newValue[iterator].key, scope.originalListTranslations[iterator].key) && angular.equals(newValue[iterator].value[0], scope.originalListTranslations[iterator].value[0])  && angular.equals(newValue[iterator].value[1], scope.originalListTranslations[iterator].value[1])) {
+                if (angular.equals(newValue[iterator + firstEntitiesNotSave].key, scope.originalListTranslations[iterator].key) && angular.equals(newValue[iterator + firstEntitiesNotSave].value[0], scope.originalListTranslations[iterator].value[0])  && angular.equals(newValue[iterator + firstEntitiesNotSave].value[1], scope.originalListTranslations[iterator].value[1])) {
                   scope.listTranslations[iterator].modified = false
                 } else {
                   scope.listTranslations[iterator].modified = true
