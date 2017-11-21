0819108347c5d10ac21d6479fee96342a60852a4
foxdog05000 <florianarmand05000@gmail.com>
2017-11-21 18:00:20 +0100

    created directives keyboard-shortcuts and theming for settings page

--- app/scripts/settings/basicSettings.js
+++ app/scripts/settings/basicSettings.js
@@ -28,20 +28,6 @@ angular.module('serinaApp').directive('basicSettings', function ($rootScope, $md
         LocalStorage.setItem($rootScope.keySettingsApp, $rootScope.settings)
       }
 
-      scope.colors = [
-        { label: 'commons.themeColor.indigo', value: 'indigo' },
-        { label: 'commons.themeColor.green', value: 'green' },
-        { label: 'commons.themeColor.orange', value: 'orange' },
-        { label: 'commons.themeColor.purple', value: 'purple' },
-        { label: 'commons.themeColor.brown', value: 'brown' },
-        { label: 'commons.themeColor.grey', value: 'grey' }
-      ]
-
-      scope.changeColor = function (color) {
-        $rootScope.settings.theme = color
-        LocalStorage.setItem($rootScope.keySettingsApp, $rootScope.settings)
-      }
-
     }
   }
 })
