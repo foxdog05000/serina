0d24a093467251450b4a3142bb077d35fc846425
foxdog05000 <florianarmand05000@gmail.com>
2018-02-02 17:04:44 +0100

    Use ng-repeat for display shortcuts list in settings page

--- app/scripts/settings/keyboardShortcuts.js
+++ app/scripts/settings/keyboardShortcuts.js
@@ -1,5 +1,23 @@
 'use strict'
 
 angular.module('serinaApp').component('keyboardShortcuts', {
+  controller: function KeyboardShortcutsCtrl () {
+
+    this.shortcuts = [
+      {
+        descriptionKey: 'toggleSearchBar',
+        key: 'F'
+      },
+      {
+        descriptionKey: 'navigateToPreviousCorrespondingItem',
+        icon: 'arrow_upward'
+      },
+      {
+        descriptionKey: 'navigateToNextCorrespondingItem',
+        icon: 'arrow_downward'
+      },
+    ];
+
+  },
   templateUrl: 'views/settings/keyboard-shortcuts.html'
 })
