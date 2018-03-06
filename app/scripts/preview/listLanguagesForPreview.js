0f109047c91b52db77529e9369ad488d9e4c89c0
Farmand <farmand@netceler.com>
2018-03-06 15:51:38 +0100

    Add dropzone on list language (page preview) for import new json language

--- app/scripts/preview/listLanguagesForPreview.js
+++ app/scripts/preview/listLanguagesForPreview.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('serinaApp').directive('listLanguagesForPreview', function ($log, DataAccessor) {
+angular.module('serinaApp').directive('listLanguagesForPreview', function ($log, $timeout, DataAccessor, Upload) {
   return {
     restrict: 'E',
     templateUrl: 'views/preview/list-languages-for-preview.html',
@@ -23,7 +23,39 @@ angular.module('serinaApp').directive('listLanguagesForPreview', function ($log,
         })
       }
 
+      scope.log = '';
+
+      scope.upload = function (files) {
+        if (files && files.length) {
+          for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            if (!file.$error) {
+              Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {
+                  file: file
+                }
+              }).then(function (resp) {
+                $timeout(function() {
+                  scope.log = 'file: ' +
+                  resp.config.data.file.name +
+                  ', Response: ' + JSON.stringify(resp.data) +
+                  '\n' + $scope.log;
+                });
+              }, null, function (evt) {
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                scope.log = 'progress: ' + progressPercentage + '% ' + evt.config.data.file.name + '\n' + scope.log;
+              });
+            }
+          }
+        }
+      }
+
       recoverListLanguagesForPreview()
+
+      scope.$watch('files', function () {
+        scope.upload(scope.files)
+      })
 
     }
   }
