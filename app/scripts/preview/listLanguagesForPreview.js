c64467de6fbbd443cc06b31d31b1f1ce24d10aca
Florian Armand <florianarmand05000@gmail.com>
2018-11-29 19:33:12 +0100

    chore: remove ; unnecessary

--- app/scripts/preview/listLanguagesForPreview.js
+++ app/scripts/preview/listLanguagesForPreview.js
@@ -23,12 +23,10 @@ angular.module('serinaApp').directive('listLanguagesForPreview', function ($log,
         })
       }
 
-      scope.log = '';
-
       scope.upload = function (files) {
         if (files && files.length) {
           for (var i = 0; i < files.length; i++) {
-            var file = files[i];
+            var file = files[i]
             if (!file.$error) {
               Upload.upload({
                 url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
@@ -36,22 +34,24 @@ angular.module('serinaApp').directive('listLanguagesForPreview', function ($log,
                   file: file
                 }
               }).then(function (resp) {
-                $timeout(function() {
+                $timeout(function () {
                   scope.log = 'file: ' +
                   resp.config.data.file.name +
                   ', Response: ' + JSON.stringify(resp.data) +
-                  '\n' + scope.log;
-                });
+                  '\n' + scope.log
+                })
               }, null, function (evt) {
-                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                scope.log = 'progress: ' + progressPercentage + '% ' + evt.config.data.file.name + '\n' + scope.log;
-              });
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total)
+                scope.log = 'progress: ' + progressPercentage + '% ' + evt.config.data.file.name + '\n' + scope.log
+              })
             }
           }
         }
       }
 
+
       recoverListLanguagesForPreview()
+      scope.log = ''
 
       scope.$watch('files', function () {
         scope.upload(scope.files)
